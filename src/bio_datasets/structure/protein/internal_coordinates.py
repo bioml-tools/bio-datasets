f631e67a01d303d3c74801a91b9c8589ad3e1e4c
alex-hh <alex.hhooker@gmail.com>
2025-01-06 14:05:13 +0200

    manual calculation of sidechain dihedrals

--- src/bio_datasets/structure/protein/internal_coordinates.py
+++ src/bio_datasets/structure/protein/internal_coordinates.py
@@ -1,8 +1,21 @@
-"""Use utils from nerfax parser to convert protein into internal coordinates."""
+"""Use utils from nerfax parser to convert protein into internal coordinates.
+
+TODO: figure out appropriate bond lengths / angles to measure with biopython
+to avoid needing to use nerfax at all for structure -> internals. (we still need
+nerfax for internals -> structure, although this could be a cool thing to contribute
+to biotite.)
+for calculation of backbone torsions, see ramachandran plot example
+phi, psi, omega = struc.dihedral_backbone(chain)
+
+n.b. backbone dihedrals involve previous residue atoms
+"""
 import jax
 import nerfax
 import numpy as np
+from biotite.structure.geometry import dihedral
+from bio_datasets.structure.protein import constants as protein_constants
 from biotite import structure as bs
+from biotite.structure.util import coord_for_atom_name_per_residue
 
 
 def load_backbone_coord_array(structure: bs.AtomArray):
@@ -28,5 +41,29 @@ def get_backbone_internals_from_atoms(structure: bs.AtomArray):
     return get_backbone_internals(xyz)
 
 
-def get_sidechain_internals(structure: bs.AtomArray):
-    pass
+def get_sidechain_internals_list(structure: bs.AtomArray) -> list:
+    # one option would be to return l, 4 array of values (+ l, 4 mask)
+    residue_torsions = []
+    # more efficient approach will be to iterate over residue types - because dihedral can operate on arrays (l,3)
+    # if we implement this, we have to also implement ordering returned dihedrals in original order.
+    for res in bs.residue_iter(structure):
+        chi_atoms_list = protein_constants.chi_angles_atoms[res.res_name[0]]
+        chis = [dihedral(*coord_for_atom_name_per_residue(res, atom_list))[0] for atom_list in chi_atoms_list]
+        residue_torsions.append(chis)
+    return residue_torsions
+
+
+def get_sidechain_internals(structure: bs.AtomArray) -> np.ndarray:
+    res_ids, res_names = bs.get_residues(structure)
+    res_names = np.array(res_names)
+    unique_res_names = np.unique(res_names)
+    chi_array = np.zeros((len(res_names), 4))
+    chi_mask = np.zeros((len(res_names), 4), dtype=bool)
+    for res_name in unique_res_names:
+        res_mask = res_names == res_name
+        chi_ids = np.argwhere(protein_constants.chi_angles_mask[protein_constants.restype_order[protein_constants.restype_3to1[res_name]]]).flatten()
+        assert len(chi_ids) == len(protein_constants.chi_angles_atoms[res_name])
+        for chi_id, chi_atom_list in zip(chi_ids, protein_constants.chi_angles_atoms[res_name]):
+            chi_array[res_mask, chi_id] = dihedral(*coord_for_atom_name_per_residue(structure[structure.res_name == res_name], chi_atom_list))
+            chi_mask[res_mask, chi_id] = True
+    return chi_array, chi_mask
